82ab2e56aee641008f9cc4bf3eaa384e00079a01
Alessio Barbiero <alessio.barbiero@dianacorp.com>
2021-05-21 15:20:57 +0200

    Updates

---
+++ precache-manifest.3a77b1d9ddb2a476213fffcffce16800.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931d12505da65fa1968cf42da5efedd6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "56bba5a1ceef30423e29",
+    "url": "./static/css/main.96a66218.chunk.css"
+  },
+  {
+    "revision": "2386c6e6690c5064d679",
+    "url": "./static/js/2.9509e125.chunk.js"
+  },
+  {
+    "revision": "9b9de423b52b798b8fc649e7d9e276fb",
+    "url": "./static/js/2.9509e125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56bba5a1ceef30423e29",
+    "url": "./static/js/main.c05a8090.chunk.js"
+  },
+  {
+    "revision": "28605eb84a8cfbe33344",
+    "url": "./static/js/runtime-main.0fdcff9d.js"
+  },
+  {
+    "revision": "e7caf93a04777664f84cb8b3801dea19",
+    "url": "./static/media/defaultBcg.e7caf93a.jpeg"
+  },
+  {
+    "revision": "e5350ef89f92a49bf2eec253307244f7",
+    "url": "./static/media/defaultBcg2.e5350ef8.jpeg"
+  },
+  {
+    "revision": "c07e5fc72d05224b93257b308c8b2846",
+    "url": "./static/media/loading-circle.c07e5fc7.gif"
+  }
+]);
